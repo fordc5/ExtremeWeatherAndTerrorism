222dd8026be7ae1b3839bd9bdf4b95bd0ced484f
Connor Ford <cbfa2016@mymail.pomona.edu>
2017-11-20 21:27:01 -0800

    reshuffling of csv's

---
+++ playground.R
@@ -0,0 +1,145 @@
+
+
+
+terrorismOriginalData <- read.csv(file="TerrorismMaster.csv", header=TRUE, sep=",")
+head(terrorismOriginalData)
+
+
+
+library(stringr)
+terrorismData <- terrorismOriginalData %>%
+  unite(date1, iyear,imonth,iday, sep="-") %>%
+  mutate(dateLub = ymd(date1)) %>%
+  filter(!is.na(dateLub)) %>%
+  mutate(isAttack = as.integer(!is.na(eventid))) %>%
+  mutate(countryParsed = toupper(country_txt)) %>%
+  group_by(country_txt) %>%
+  mutate(totalPerCountry = n()) %>%
+  arrange(dateLub) 
+head(terrorismData)
+dim(terrorismData)
+
+
+weatherOriginalData <- read.csv(file="GlobalLandTemperaturesByCity.csv", header=TRUE, sep=",")
+head(weatherOriginalData)
+
+
+weatherData <- weatherOriginalData %>%
+  mutate(dateLub = ydm(dt)) %>%
+  filter(!is.na(dateLub)) %>%
+  filter(dateLub >= as.Date("1970-01-01")) %>%
+  filter(!is.na(AverageTemperature)) %>%
+  mutate(Country = toupper(Country))
+
+
+dim(weatherData)
+head(weatherData, 1000)
+
+
+mergedMasterData <- left_join(weatherData, terrorismData, by=c("dateLub", "Country" = "countryParsed")) %>%
+  mutate(isAttack = as.integer(!(is.na(isAttack)))) %>%
+  arrange(dateLub)
+
+mergedMasterDataSelectedCols <- mergedMasterData[,c("dateLub", "Country", "City", "city", "AverageTemperature", "Latitude", "Longitude", "summary", "nkill", "isAttack", "eventid","totalPerCountry")] 
+
+head(mergedMasterDataSelectedCols, 100)
+dim(mergedMasterDataSelectedCols)
+
+
+
+##NN data aggregating
+nnData <- mergedMasterDataSelectedCols %>%
+  #Get country code
+  transform(countryCode = as.numeric(factor(Country))) %>%
+  #Get histDayAvg
+  transform(year = year(ymd(dateLub))) %>%
+  transform(month = month(ymd(dateLub))) %>%
+  transform(day = day(ymd(dateLub))) %>%
+  group_by_(.dots=c("countryCode","month","day")) %>% 
+  mutate(histDayAvg = mean(AverageTemperature))
+
+
+#number attacks per period function
+numTrBw <- function(frame) {
+  
+  frame <- frame %>%
+    filter(isAttack) %>%
+    filter(!is.na(city))
+  
+  
+  if (FALSE){
+    print(frame)
+    c <- vector()
+    for(i in 1:nrow(frame)) {
+      print(i)
+      row <- frame[i,]
+      # do stuff with row
+      code <- row$countryCode
+      dateFuture <- row$dateLub
+      datePast <- row$monthBefore
+      
+      temp <- frame %>% filter(countryCode == code)
+      
+      total <- 0
+      while(year(ymd(dateFuture)) != year(ymd(datePast)) | month(ymd(dateFuture)) != month(ymd(datePast)) | day(ymd(dateFuture)) != day(ymd(datePast))) {
+        #print(1)
+        prevDay <- dateFuture - days(1)
+        temp <- temp %>% filter(dateLub == prevDay) %>% slice(1)
+        print(row)
+        if(temp$isAttack) {
+          #print(2)
+          total <- total + 1
+        }
+        #print(3)
+        dateFuture <- prevDay
+      }
+      #print(4)
+      c <- append(c, total)
+      
+    }
+    print(frame %>% mutate(hellloe = c))
+  }
+}
+
+
+nnData <- nnData %>%
+  #Attacks in one month prior
+  #group_by(countryCode) %>%
+  mutate(monthBefore = dateLub - months(1))
+
+
+
+nnData <- nnData %>%
+  filter(isAttack == 1) %>%
+  #filter(!is.na(city)) %>%
+  group_by_(.dots=c("countryCode","dateLub")) %>% 
+  mutate(attacksPriorYear = n())
+
+nnData <- nnData[,c("countryCode", "histDayAvg", "AverageTemperature","isAttack", "attacksPriorYear")]
+
+
+dim(nnData)
+head(nnData,200)
+
+
+
+
+
+# Random sampling
+samplesize <- 0.60 * nrow(mergedMasterDataSelectedCols)
+set.seed(47)
+index <- sample(seq_len(nrow(mergedMasterDataSelectedCols)), size = samplesize)
+
+# Create training and test set
+dataTrain <- mergedMasterDataSelectedCols[index,]
+dataTest <- mergedMasterDataSelectedCols[-index,]
+
+# Scale data for neural network
+maxScale <- apply(mergedMasterDataSelectedCols, 2, max)
+minScale <- apply(mergedMasterDataSelectedCols, 2, min)
+scaledData <- as.data.frame(scale(mergedMasterDataSelectedCols, center = minScale, scale = maxScale - minScale))
+
+
+
+
+
